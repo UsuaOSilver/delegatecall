109da923d7604d9445bd6a4bcbeeae0f1f69bf2e
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-11-05 16:19:33 +0700

    alternate codebase

--- contracts/UseDelegatecall.sol
+++ contracts/UseDelegatecall.sol
@@ -1,17 +1,17 @@
-
+ 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
+ 
 contract ContractA {
-    
-    string internal tokenName = "RealityToken";
-    
+   
+    string internal tokenSymbol = "LVI";
+   
     function initialize() external {
         address contractBAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
         (bool success, bytes memory returndata) = contractBAddress.delegatecall(
-            abi.encodeWithSelector(ContractB.setTokenName.selector, "HumanToken")
+            abi.encodeWithSelector(ContractB.setTokenSymbol.selector, "SSN")
         );
-        
+       
         // if the function call reverted
         if (success == false) {
             // if there's a return reason string
@@ -27,12 +27,12 @@ contract ContractA {
         }
     }
 }
-
+ 
 contract ContractB {
-    
-    string internal tokenName = "HumanToken";
-    
-    function setTokenName(string calldata _newName) external {
-        tokenName = _newName;
+   
+    string internal tokenSymbol = "SSN";
+   
+    function setTokenSymbol(string calldata _newSymbol) external {
+        tokenSymbol = _newSymbol;
     }
 }
